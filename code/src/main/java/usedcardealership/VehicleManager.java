a19fbcfad41cc145accd7237334f33a55c9d797b
talonlikeaclaw <talonlikeaclaw@proton.me>
2024-11-04 17:13:08 -0500

    create VehicleManager skeleton class

---
+++ code/src/main/java/usedcardealership/VehicleManager.java
@@ -0,0 +1,51 @@
+package usedcardealership;
+import java.util.*;
+
+public class VehicleManager {
+  private List<Vehicle> inventory;
+  private List<Vehicle> database;
+
+
+  /**
+   * VehicleManager Constructor
+   * Initializes the dealership's inventory and database of vehicles.
+   * @param inventory list of vehicles dealership currently has in inventory.
+   * @param database list of vehicles dealership has seen before.
+   * @constructor
+   */
+  public VehicleManager(List<Vehicle> inventory, List<Vehicle> database) {
+    throw new UnsupportedOperationException("Not written yet");
+  }
+
+  /**
+   * Method for adding a vehicle to the dealership's inventory or database.
+   * @param v the vehicle that is being added.
+   */
+  public void addVehicle(Vehicle v) {
+    throw new UnsupportedOperationException("Not written yet");
+  }
+
+  /**
+   * Method that removes a vehicle from the dealership's inventory.
+   * @param v the vehcile that is being removed.
+   */
+  public void removeVehicle(Vehicle v) {
+    throw new UnsupportedOperationException("Not written yet");
+  }
+
+  /** 
+   * Method that searches for a vehicle in the inventory or database.
+   * @param v vehicle that we are searching for.
+   */
+  public void searchVehicle(Vehicle v) {
+    throw new UnsupportedOperationException("Not written yet");
+  }
+
+  /**
+   * Method that allows us to update a vehicles mutable fields.
+   * @param v vehicle that we are updating the mutable fields of.
+   */
+  public void updateVehicle(Vehicle v) {
+    throw new UnsupportedOperationException("Not written yet");
+  }
+}
