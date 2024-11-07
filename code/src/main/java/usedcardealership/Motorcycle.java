bf241a3658d55d61c99a14a3cdf3a80a2e2aebe3
talonlikeaclaw <talonlikeaclaw@proton.me>
2024-11-06 19:14:20 -0500

    standardize indentation and add descriptive class comments

--- code/src/main/java/usedcardealership/Motorcycle.java
+++ code/src/main/java/usedcardealership/Motorcycle.java
@@ -1,5 +1,5 @@
 /**
- * Motorcycle type.
+ * Represents Motorcycle objects.
  * 
  * @author Talon Dunbar - 2131651
  * @version 11/6/2024
@@ -8,47 +8,46 @@
 package usedcardealership;
 
 public class Motorcycle extends Vehicle {
-  private double engineCC;
-  private String handlebarType;
+    private double engineCC;
+    private String handlebarType;
 
-  /**
-   * Motorcycle Constructor
-   * Initializes the Motorcycle fields
-   * 
-   * @param id            the Motorcycle's unique identifier
-   * @param make          the company that makes the Motorcycle
-   * @param model         the name of the Motorcycle's model
-   * @param year          the year the Motorcycle was released
-   * @param price         the initial price of the Motorcycle
-   * @param color         the color of the Motorcycle's paint
-   * @param transmission  the type of transmission (Auto, Manual, CVT)
-   * @param driveType     the drive type of the Motorcycle
-   * @param horsepower    the Motorcycle's engine's horsepower
-   * @param weight        the weight of the Motorcycle
-   * @param mileage       the number of kilometers the Motorcycle has on the gauge
-   * @param damage        the damage of the Motorcycle (00.00 - 100.00)
-   * @param isElectric    if the Motorcycle is electric of not
-   * @param engineCC      the Motorcycle engine's capcity
-   * @param handlebarType the Motorcycle's handlebar type
-   */
-  public Motorcycle(int id, String make, String model, int year, double price, String color, String transmission,
-      String driveType, int horsepower, double weight, double mileage, double damage, boolean isElectric,
-      double engineCC, String handlebarType) {
-    super(id, make, model, year, price, color, transmission, driveType, horsepower, weight, mileage, damage,
-        isElectric);
-    throw new UnsupportedOperationException("Not written yet");
-  }
+    /**
+     * Motorcycle Constructor
+     * Initializes the Motorcycle fields
+     * 
+     * @param id            the Motorcycle's unique identifier
+     * @param make          the company that makes the Motorcycle
+     * @param model         the name of the Motorcycle's model
+     * @param year          the year the Motorcycle was released
+     * @param price         the initial price of the Motorcycle
+     * @param color         the color of the Motorcycle's paint
+     * @param transmission  the type of transmission (Auto, Manual, CVT)
+     * @param driveType     the drive type of the Motorcycle
+     * @param horsepower    the Motorcycle's engine's horsepower
+     * @param weight        the weight of the Motorcycle
+     * @param mileage       the number of kilometers the Motorcycle has on the gauge
+     * @param damage        the damage of the Motorcycle (00.00 - 100.00)
+     * @param isElectric    if the Motorcycle is electric of not
+     * @param engineCC      the Motorcycle engine's capcity
+     * @param handlebarType the Motorcycle's handlebar type
+     */
+    public Motorcycle(int id, String make, String model, int year, double price, String color, String transmission,
+            String driveType, int horsepower, double weight, double mileage, double damage, boolean isElectric,
+            double engineCC, String handlebarType) {
+        super(id, make, model, year, price, color, transmission, driveType, horsepower, weight, mileage, damage, isElectric);
+        throw new UnsupportedOperationException("Not written yet");
+    }
 
-  @Override
-  public String toString() {
-    throw new UnsupportedOperationException("Not written yet");
-  }
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException("Not written yet");
+    }
 
-  public double getEngineCC() {
-    throw new UnsupportedOperationException("Not written yet");
-  }
+    public double getEngineCC() {
+        throw new UnsupportedOperationException("Not written yet");
+    }
 
-  public String getHandleType() {
-    throw new UnsupportedOperationException("Not written yet");
-  }
+    public String getHandleType() {
+        throw new UnsupportedOperationException("Not written yet");
+    }
 }
--- code/src/main/java/usedcardealership/RV.java
+++ code/src/main/java/usedcardealership/RV.java
@@ -1,5 +1,5 @@
 /**
- * RV type.
+ * Represents Recreational Vehicle objects,
  * 
  * @author Talon Dunbar - 2131651
  * @version 11/6/2024
@@ -8,50 +8,50 @@
 package usedcardealership;
 
 public class RV extends EnclosedVehicle {
-  private int sleepCapacity;
-  private boolean hasBathroom;
+    private int sleepCapacity;
+    private boolean hasBathroom;
 
-  /**
-   * RV Constructor
-   * Initializes the RV fields
-   * 
-   * @param id            the RV's unique identifier
-   * @param make          the company that makes the RV
-   * @param model         the name of the RV's model
-   * @param year          the year the RV was released
-   * @param price         the initial price of the RV
-   * @param color         the color of the RV's paint
-   * @param transmission  the type of transmission (Auto, Manual, CVT)
-   * @param driveType     the drive type of the RV
-   * @param horsepower    the RV's engine's horsepower
-   * @param weight        the weight of the RV
-   * @param mileage       the number of kilometers the RV has
-   * @param damage        the damage of the RV (00.00 - 100.00)
-   * @param isElectric    if the RV is electric of not
-   * @param numSeats      the number of seats of the RV
-   * @param numDoor       the number of doors of the RV
-   * @param hasSunRoof    if the RV has a sunroof or not
-   * @param sleepCapacity the amount of people who can sleep in the RV
-   * @param hasBathroom   if the RV has a bathroom or not
-   */
-  public RV(int id, String make, String model, int year, double price, String color, String transmission,
-      String driveType, int horsepower, double weight, double mileage, double damage, boolean isElectric, int numSeats,
-      int numDoors, boolean hasSunRoof, int sleepCapacity, boolean hasBathroom) {
-    super(id, make, model, year, price, color, transmission, driveType, horsepower, weight, mileage, damage, isElectric,
-        numSeats, numDoors, hasSunRoof);
-    throw new UnsupportedOperationException("Not written yet");
-  }
+    /**
+     * RV Constructor
+     * Initializes the RV fields
+     * 
+     * @param id            the RV's unique identifier
+     * @param make          the company that makes the RV
+     * @param model         the name of the RV's model
+     * @param year          the year the RV was released
+     * @param price         the initial price of the RV
+     * @param color         the color of the RV's paint
+     * @param transmission  the type of transmission (Auto, Manual, CVT)
+     * @param driveType     the drive type of the RV
+     * @param horsepower    the RV's engine's horsepower
+     * @param weight        the weight of the RV
+     * @param mileage       the number of kilometers the RV has
+     * @param damage        the damage of the RV (00.00 - 100.00)
+     * @param isElectric    if the RV is electric of not
+     * @param numSeats      the number of seats of the RV
+     * @param numDoor       the number of doors of the RV
+     * @param hasSunRoof    if the RV has a sunroof or not
+     * @param sleepCapacity the amount of people who can sleep in the RV
+     * @param hasBathroom   if the RV has a bathroom or not
+     */
+    public RV(int id, String make, String model, int year, double price, String color, String transmission,
+            String driveType, int horsepower, double weight, double mileage, double damage, boolean isElectric,
+            int numSeats, int numDoors, boolean hasSunRoof, int sleepCapacity, boolean hasBathroom) {
+        super(id, make, model, year, price, color, transmission, driveType, horsepower, weight, mileage, damage,
+                isElectric, numSeats, numDoors, hasSunRoof);
+        throw new UnsupportedOperationException("Not written yet");
+    }
 
-  @Override
-  public String toString() {
-    throw new UnsupportedOperationException("Not written yet");
-  }
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException("Not written yet");
+    }
 
-  public int getSleepCapacity() {
-    throw new UnsupportedOperationException("Not written yet");
-  }
+    public int getSleepCapacity() {
+        throw new UnsupportedOperationException("Not written yet");
+    }
 
-  public boolean getHasBathroom() {
-    throw new UnsupportedOperationException("Not written yet");
-  }
+    public boolean getHasBathroom() {
+        throw new UnsupportedOperationException("Not written yet");
+    }
 }
