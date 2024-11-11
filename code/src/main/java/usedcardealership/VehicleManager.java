840c22e172d75a066f97593f2448efedbeb28b7f
talonlikeaclaw <talonlikeaclaw@proton.me>
2024-11-10 21:32:09 -0500

    implement copy constructors for vehicle update function

--- code/src/main/java/usedcardealership/VehicleManager.java
+++ code/src/main/java/usedcardealership/VehicleManager.java
@@ -6,6 +6,7 @@
  */
 
 package usedcardealership;
+
 import usedcardealership.Vehicle;
 import java.util.*;
 
@@ -13,13 +14,12 @@ public class VehicleManager {
   private List<Vehicle> inventory;
   private List<Vehicle> database;
 
-
   /**
    * VehicleManager Constructor
    * Initializes the dealership's inventory and database of vehicles.
    * 
    * @param inventory list of vehicles dealership currently has in inventory.
-   * @param database list of vehicles dealership has seen before.
+   * @param database  list of vehicles dealership has seen before.
    */
   public VehicleManager(List<Vehicle> inventory, List<Vehicle> database) {
     this.inventory = inventory;
@@ -28,6 +28,7 @@ public class VehicleManager {
 
   /**
    * Method for adding a vehicle to the dealership's inventory or database.
+   * Only adds if it doesn't already exitst.
    * 
    * @param v the vehicle that is being added.
    */
@@ -49,8 +50,8 @@ public class VehicleManager {
     inventory.remove(v);
   }
 
-  /** 
-   * Method that searches for vehciles in the inventory based on specific criteria.
+  /**
+   * Method that searches for vehicles in the inventory based on criteria.
    * 
    * @param criteria a IFilter sub-type object to determine filtering criteria.
    * @return List<Vehicle> list of filtered vehicles based on criteria input.
@@ -59,23 +60,7 @@ public class VehicleManager {
     List<Vehicle> result = new ArrayList<>();
     for (Vehicle v : inventory) {
       // TODO: Need to create IFilter Strategy with matches overrides for criteria
-      if (criteria.matches(v))  {
-        result.add(v);
-      }
-    }
-    return result;
-  }
-
-  /** 
-   * Method that searches for a vehicles in the database based on specific criteria.
-   * 
-   * @param criteria a IFilter sub-type object
-   * @return List<Vehicle> list of filtered vehicles based on criteria input.
-   */
-  public List<Vehicle> searchDatabase(IFilter criteria) {
-    List<Vehicle> result = new ArrayList<>();
-    for (Vehicle v : database) {
-      if (criteria.matches(v))  {
+      if (criteria.matches(v)) {
         result.add(v);
       }
     }
@@ -83,7 +68,23 @@ public class VehicleManager {
   }
 
   /**
-   * Method that allows us to update a vehicles mutable fields in both inventory and database.
+   * Method that searches for a vehicles in the database based on criteria.
+   * 
+   * @param criteria a IFilter sub-type object
+   * @return List<Vehicle> list of filtered vehicles based on criteria input.
+   */
+  public List<Vehicle> searchDatabase(IFilter criteria) {
+    List<Vehicle> result = new ArrayList<>();
+    for (Vehicle v : database) {
+      if (criteria.matches(v)) {
+        result.add(v);
+      }
+    }
+    return result;
+  }
+
+  /**
+   * Method that allows us to update a vehicles mutable fields in Lists
    * 
    * @param v vehicle with updated data.
    */
@@ -95,14 +96,27 @@ public class VehicleManager {
   /**
    * Helper method that updates a vehicle's information within a given list.
    * 
-   * @param vehicleList the list of vehicles.
    * @param updatedVehicle the vehicle with updated data.
    */
   private void updateVehicleInList(List<Vehicle> vehicleList, Vehicle updatedVehicle) {
     for (int i = 0; i < vehicleList.size(); i++) {
       if (vehicleList.get(i).equals(updatedVehicle)) {
-        // TODO: Create copy constructor in Vehicle
-        vehicleList.set(i, new Vehicle(updatedVehicle));
+        // Check the type of updatedVehicle and call correct copy constructor
+        if (updatedVehicle instanceof Car) {
+          vehicleList.set(i, new Car((Car) updatedVehicle));
+        } else if (updatedVehicle instanceof SUV) {
+          vehicleList.set(i, new SUV((SUV) updatedVehicle));
+        } else if (updatedVehicle instanceof Van) {
+          vehicleList.set(i, new Van((Van) updatedVehicle));
+        } else if (updatedVehicle instanceof PickupTruck) {
+          vehicleList.set(i, new PickupTruck((PickupTruck) updatedVehicle));
+        } else if (updatedVehicle instanceof Motorcycle) {
+          vehicleList.set(i, new Motorcycle((Motorcycle) updatedVehicle));
+        } else if (updatedVehicle instanceof RV) {
+          vehicleList.set(i, new RV((RV) updatedVehicle));
+        } else {
+          throw new IllegalArgumentException("Unsupported vehicle type.");
+        }
         break;
       }
     }
