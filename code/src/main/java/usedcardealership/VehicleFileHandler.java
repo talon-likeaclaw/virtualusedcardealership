729b762520829906cb569a07bcf5b63958230790
talonlikeaclaw <talonlikeaclaw@proton.me>
2024-11-12 13:39:43 -0500

    write all of the js doc comments

--- code/src/main/java/usedcardealership/VehicleFileHandler.java
+++ code/src/main/java/usedcardealership/VehicleFileHandler.java
@@ -15,10 +15,21 @@ import java.util.*;
 public class VehicleFileHandler implements IDataHandler<Vehicle> {
   private final Path filePath;
 
+  /**
+   * Constructs a VehicleFileHandler with the specified file path.
+   * 
+   * @param String filePath - The path to the file containing vehicle data.
+   * @throws IOException
+   */
   public VehicleFileHandler(String filePath) throws IOException {
     this.filePath = Paths.get(filePath);
   }
 
+  /**
+   * Loads vehicles from the file and returns them as a list.
+   * 
+   * @return List<Vehicle> - A list of vehicles loaded from the file.
+   */
   @Override
   public List<Vehicle> load() {
     List<Vehicle> vehicles = new ArrayList<>();
@@ -37,6 +48,13 @@ public class VehicleFileHandler implements IDataHandler<Vehicle> {
     return vehicles;
   }
 
+  /**
+   * Creates a Vehicle based on the type and field data.
+   * 
+   * @param String   type - The type of the vehicle.
+   * @param String[] fields - The field data for the vehicle.
+   * @return Vehicle - The created Vehicle instance or null if type not valid.
+   */
   private Vehicle createVehicle(String type, String[] fields) {
     int id = Integer.parseInt(fields[1]);
     String make = fields[2];
@@ -75,6 +93,25 @@ public class VehicleFileHandler implements IDataHandler<Vehicle> {
     }
   }
 
+  /**
+   * Helper method to create a Motorcycle.
+   * 
+   * @param fields       String array of all fields
+   * @param id           the Motorcycle's unique identifier
+   * @param make         the company that makes the Motorcycle
+   * @param model        the name of the Motorcycle's model
+   * @param year         the year the Motorcycle was released
+   * @param price        the initial price of the Motorcycle
+   * @param color        the color of the Motorcycle's paint
+   * @param transmission the type of transmission (Auto, Manual)
+   * @param driveType    the drive type of the Motorcycle
+   * @param horsepower   the Motorcycle's engine's horsepower
+   * @param weight       the weight of the Motorcycle
+   * @param kilometerage the number of kilometers the Motorcycle has on the gauge
+   * @param damage       the damage of the Motorcycle (00.00 - 100.00)
+   * @param isElectric   if the Motorcycle is electric of no
+   * @return Motorcycle - The created Motorcycle instance.
+   */
   private Motorcycle createMotorcycle(
       String[] fields,
       int id,
@@ -96,6 +133,25 @@ public class VehicleFileHandler implements IDataHandler<Vehicle> {
         weight, kilometerage, damage, isElectric, engineCC, handlebarType);
   }
 
+  /**
+   * Helper method to create a RV.
+   * 
+   * @param fields       String array of all fields
+   * @param id           the RV's unique identifier
+   * @param make         the company that makes the RV
+   * @param model        the name of the RV's model
+   * @param year         the year the RV was released
+   * @param price        the initial price of the RV
+   * @param color        the color of the RV's paint
+   * @param transmission the type of transmission (Auto, Manual)
+   * @param driveType    the drive type of the RV
+   * @param horsepower   the RV's engine's horsepower
+   * @param weight       the weight of the RV
+   * @param kilometerage the number of kilometers the RV has on the gauge
+   * @param damage       the damage of the RV (00.00 - 100.00)
+   * @param isElectric   if the RV is electric of no
+   * @return RV - The created RV instance.
+   */
   private RV createRV(
       String[] fields,
       int id,
@@ -120,6 +176,25 @@ public class VehicleFileHandler implements IDataHandler<Vehicle> {
         weight, kilometerage, damage, isElectric, numSeats, numDoors, hasSunRoof, sleepCapacity, hasBathroom);
   }
 
+  /**
+   * Helper method to create a Car.
+   * 
+   * @param fields       String array of all fields
+   * @param id           the Car's unique identifier
+   * @param make         the company that makes the Car
+   * @param model        the name of the Car's model
+   * @param year         the year the Car was released
+   * @param price        the initial price of the Car
+   * @param color        the color of the Car's paint
+   * @param transmission the type of transmission (Auto, Manual)
+   * @param driveType    the drive type of the Car
+   * @param horsepower   the Car's engine's horsepower
+   * @param weight       the weight of the Car
+   * @param kilometerage the number of kilometers the Car has on the gauge
+   * @param damage       the damage of the Car (00.00 - 100.00)
+   * @param isElectric   if the Car is electric of no
+   * @return Car - The created Car instance.
+   */
   private Car createCar(
       String[] fields,
       int id,
@@ -143,6 +218,25 @@ public class VehicleFileHandler implements IDataHandler<Vehicle> {
         weight, kilometerage, damage, isElectric, numSeats, numDoors, hasSunRoof, isConvertible);
   }
 
+  /**
+   * Helper method to create a SUV.
+   * 
+   * @param fields       String array of all fields
+   * @param id           the SUV's unique identifier
+   * @param make         the company that makes the SUV
+   * @param model        the name of the SUV's model
+   * @param year         the year the SUV was released
+   * @param price        the initial price of the SUV
+   * @param color        the color of the SUV's paint
+   * @param transmission the type of transmission (Auto, Manual)
+   * @param driveType    the drive type of the SUV
+   * @param horsepower   the SUV's engine's horsepower
+   * @param weight       the weight of the SUV
+   * @param kilometerage the number of kilometers the SUV has on the gauge
+   * @param damage       the damage of the SUV (00.00 - 100.00)
+   * @param isElectric   if the SUV is electric of no
+   * @return SUV - The created SUV instance.
+   */
   private SUV createSUV(
       String[] fields,
       int id,
@@ -166,6 +260,25 @@ public class VehicleFileHandler implements IDataHandler<Vehicle> {
         weight, kilometerage, damage, isElectric, numSeats, numDoors, hasSunRoof, hasThirdRowSeating);
   }
 
+  /**
+   * Helper method to create a PickupTruck.
+   * 
+   * @param fields       String array of all fields
+   * @param id           the PickupTruck's unique identifier
+   * @param make         the company that makes the PickupTruck
+   * @param model        the name of the PickupTruck's model
+   * @param year         the year the PickupTruck was released
+   * @param price        the initial price of the PickupTruck
+   * @param color        the color of the PickupTruck's paint
+   * @param transmission the type of transmission (Auto, Manual)
+   * @param driveType    the drive type of the PickupTruck
+   * @param horsepower   the PickupTruck's engine's horsepower
+   * @param weight       the weight of the PickupTruck
+   * @param kilometerage the number of kilometers the PickupTruck has on the gauge
+   * @param damage       the damage of the PickupTruck (00.00 - 100.00)
+   * @param isElectric   if the PickupTruck is electric of no
+   * @return PickupTruck - The created PickupTruck instance.
+   */
   private PickupTruck createPickupTruck(
       String[] fields,
       int id,
@@ -192,6 +305,25 @@ public class VehicleFileHandler implements IDataHandler<Vehicle> {
         cargoCapacity, bedLength, towingCapacity);
   }
 
+  /**
+   * Helper method to create a Van.
+   * 
+   * @param fields       String array of all fields
+   * @param id           the Van's unique identifier
+   * @param make         the company that makes the Van
+   * @param model        the name of the Van's model
+   * @param year         the year the Van was released
+   * @param price        the initial price of the Van
+   * @param color        the color of the Van's paint
+   * @param transmission the type of transmission (Auto, Manual)
+   * @param driveType    the drive type of the Van
+   * @param horsepower   the Van's engine's horsepower
+   * @param weight       the weight of the Van
+   * @param kilometerage the number of kilometers the Van has on the gauge
+   * @param damage       the damage of the Van (00.00 - 100.00)
+   * @param isElectric   if the Van is electric of no
+   * @return Van - The created Van instance.
+   */
   private Van createVan(
       String[] fields,
       int id,
