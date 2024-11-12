c4977bc85b8c221e3359f31d9ea8cca8f7f4219a
talonlikeaclaw <talonlikeaclaw@proton.me>
2024-11-12 12:50:03 -0500

    implement IDataHandler interface

---
+++ code/src/main/java/usedcardealership/IDataHandler.java
@@ -0,0 +1,28 @@
+
+/**
+ * Interface representing a generic data handler strategy for loading and saving items.
+ *
+ * @param <T> the type of items to handle
+ * @author Talon Dunbar
+ * @version 11/12/2024
+ */
+
+package usedcardealership;
+
+import java.util.*;
+
+public interface IDataHandler<T> {
+    /**
+     * Loads a list of items from the data source.
+     * 
+     * @return a List of items loaded from the data source
+     */
+    List<T> load();
+
+    /**
+     * Saves a list of items to the data source.
+     *
+     * @param items a List of items to be saved to the data source
+     */
+    void save(List<T> items);
+}
