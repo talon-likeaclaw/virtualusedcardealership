bf81d7fdaa17b3db40547cf6592de432762aaa20
talonlikeaclaw <talonlikeaclaw@proton.me>
2024-11-12 13:12:19 -0500

    add logic for creating each Vehicle subtype from String[] fields

--- code/src/main/java/usedcardealership/VehicleFileHandler.java
+++ code/src/main/java/usedcardealership/VehicleFileHandler.java
@@ -14,7 +14,7 @@ import java.util.*;
 
 public class VehicleFileHandler implements IDataHandler<Vehicle> {
   private final Path filePath;
-  
+
   public VehicleFileHandler(String filePath) throws IOException {
     this.filePath = Paths.get(filePath);
   }
@@ -29,7 +29,6 @@ public class VehicleFileHandler implements IDataHandler<Vehicle> {
         String[] fields = line.split(",");
         String type = fields[0];
 
-        // TODO: Create helper method(s) for each Vehicle type
         Vehicle vehicle = createVehicle(type, fields);
         vehicles.add(vehicle);
       }
@@ -37,6 +36,70 @@ public class VehicleFileHandler implements IDataHandler<Vehicle> {
       e.printStackTrace();
     }
     return vehicles;
+  }
+
+  private Vehicle createVehicle(String type, String[] fields) {
+    int id = Integer.parseInt(fields[1]);
+    String make = fields[2];
+    String model = fields[3];
+    int year = Integer.parseInt(fields[4]);
+    double price = Double.parseDouble(fields[5]);
+    String color = fields[6];
+    String transmission = fields[7];
+    String driveType = fields[8];
+    int horsepower = Integer.parseInt(fields[9]);
+    double weight = Double.parseDouble(fields[10]);
+    double kilometerage = Double.parseDouble(fields[11]);
+    double damage = Double.parseDouble(fields[12]);
+    boolean isElectric = Boolean.parseBoolean(fields[13]);
+
+    if (type == "Motorcycle") {
+      double engineCC = Double.parseDouble(fields[14]);
+      String handlebarType = fields[15];
+      return new Motorcycle(id, make, model, year, price, color, transmission, driveType,
+          horsepower, weight, kilometerage, damage, isElectric, engineCC, handlebarType);
+    } else if (type == "RV") {
+      int numSeats = Integer.parseInt(fields[14]);
+      int numDoors = Integer.parseInt(fields[15]);
+      boolean hasSunRoof = Boolean.parseBoolean(fields[16]);
+      int sleepCapacity = Integer.parseInt(fields[17]);
+      boolean hasBathroom = Boolean.parseBoolean(fields[18]);
+      return new RV(id, make, model, year, price, color, transmission, driveType, horsepower, weight,
+          kilometerage, damage, isElectric, numSeats, numDoors, hasSunRoof, sleepCapacity, hasBathroom);
+    } else if (type == "Car") {
+      int numSeats = Integer.parseInt(fields[14]);
+      int numDoors = Integer.parseInt(fields[15]);
+      boolean hasSunRoof = Boolean.parseBoolean(fields[16]);
+      boolean isConvertible = Boolean.parseBoolean(fields[17]);
+      return new Car(id, make, model, year, price, color, transmission, driveType, horsepower,
+          weight, kilometerage, damage, isElectric, numSeats, numDoors, hasSunRoof, isConvertible);
+    } else if (type == "SUV") {
+      int numSeats = Integer.parseInt(fields[14]);
+      int numDoors = Integer.parseInt(fields[15]);
+      boolean hasSunRoof = Boolean.parseBoolean(fields[16]);
+      boolean hasThirdRowSeating = Boolean.parseBoolean(fields[17]);
+      return new SUV(id, make, model, year, price, color, transmission, driveType, horsepower, weight,
+          kilometerage, damage, isElectric, numSeats, numDoors, hasSunRoof, hasThirdRowSeating);
+    } else if (type == "PickupTruck") {
+      int numSeats = Integer.parseInt(fields[14]);
+      int numDoors = Integer.parseInt(fields[15]);
+      boolean hasSunRoof = Boolean.parseBoolean(fields[16]);
+      double cargoCapacity = Double.parseDouble(fields[17]);
+      double bedLength = Double.parseDouble(fields[18]);
+      double towingCapacity = Double.parseDouble(fields[19]);
+      return new PickupTruck(id, make, model, year, price, color, transmission, driveType, horsepower, weight,
+          kilometerage, damage, isElectric, numSeats, numDoors, hasSunRoof, cargoCapacity, bedLength, towingCapacity);
+    } else if (type == "Van") {
+      int numSeats = Integer.parseInt(fields[14]);
+      int numDoors = Integer.parseInt(fields[15]);
+      boolean hasSunRoof = Boolean.parseBoolean(fields[16]);
+      double cargoCapacity = Double.parseDouble(fields[17]);
+      boolean hasSlidingDoors = Boolean.parseBoolean(fields[18]);
+      return new Van(id, make, model, year, price, color, transmission, driveType, horsepower, weight,
+          kilometerage, damage, isElectric, numSeats, numDoors, hasSunRoof, cargoCapacity, hasSlidingDoors);
+    } else {
+      return null;
+    }
   }
 
   @Override
