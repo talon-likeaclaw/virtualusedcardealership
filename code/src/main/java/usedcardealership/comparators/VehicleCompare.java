1cf36ab09009ca5035ed3a06b3198f8276353d25
unknown <jseb1401@gmail.com>
2024-11-18 17:06:53 -0500

    Add comparator type and subtypes

---
+++ code/src/main/java/usedcardealership/comparators/VehicleCompare.java
@@ -0,0 +1,21 @@
+/**
+ * Compare abstract class
+ * 
+ * @author Juan Badel Sebastian - 2338127
+ * @version 11/18/2024
+ */
+package usedcardealership.comparators;
+import usedcardealership.data.vehicle.*;
+
+/**
+ * Compares two Vehicles, used for sorting.
+ * 
+ * @param vehicle1 vehicle that gets compared to
+ * @param vehicle2 vehicle to compare with
+ * @return int
+ */
+public abstract class VehicleCompare implements ICompare<Vehicle>{
+
+    @Override
+    public abstract int compareTo(Vehicle vehicle1, Vehicle vehicle2);
+}
