83cf09adafe1d779749b109753412b19853191fe
talonlikeaclaw <talonlikeaclaw@proton.me>
2024-11-19 16:42:31 -0500

    implement view for choosing between vehicle types

--- code/src/main/java/usedcardealership/UsedCarDealership.java
+++ code/src/main/java/usedcardealership/UsedCarDealership.java
@@ -10,6 +10,7 @@ import usedcardealership.business.manager.*;
 
 public class UsedCarDealership {
     private static Prompter prompter;
+
     public static void main(String[] args) {
         prompter = new Prompter();
         DealershipManager dealership = initialize();
@@ -17,24 +18,70 @@ public class UsedCarDealership {
         shutdown();
     }
 
+    /**
+     * Main menu that allows user to choose what to do
+     * 
+     * @param dealership the DealershipManager object
+     */
     private static void mainMenuView(DealershipManager dealership) {
         boolean inPage = true;
         System.out.println("Welcome to " + dealership.getName() + "!");
         System.out.println("\nPlease select an option:");
         while (inPage) {
             switch (prompter.promptOption(
-                    "1: Browse Vehicles.\n2: View Account and Owned Vehicles.\n3: Sell Vehicle to Dealership.\n0: Exit.", 3)) {
+                    "1: Browse Vehicles\n2: View Account and Owned Vehicles\n3: Sell Vehicle to Dealership\n0: Exit",
+                    3)) {
                 case 0:
                     inPage = false;
                     break;
                 case 1:
-                    // browseVehicleView()
+                    browseVehiclesView(dealership);
                     break;
                 case 2:
                     // viewAccountView()
                     break;
                 case 3:
                     // sellVehicleView()
+                    break;
+            }
+        }
+    }
+
+    /**
+     * Menu that allows user to choose between vehicle type
+     * 
+     * @param dealership the DealershipManager object
+     */
+    private static void browseVehiclesView(DealershipManager dealership) {
+        boolean inPage = true;
+        System.out.println("\nSelect Vehicle Type:");
+        while (inPage) {
+            switch (prompter.promptOption(
+                    "1: Car\n2: SUV\n3: Pickup Truck\n4: Van\n5: RV\n6: Motorcycle\n7: All\n0: Return to Main Menu",
+                    7)) {
+                case 0:
+                    inPage = false;
+                    break;
+                case 1:
+                    // viewCars()
+                    break;
+                case 2:
+                    // viewSUVs()
+                    break;
+                case 3:
+                    // viewTrucks()
+                    break;
+                case 4:
+                    // viewVans()
+                    break;
+                case 5:
+                    // viewRVs()
+                    break;
+                case 6:
+                    // viewMotorcycles()
+                    break;
+                case 7:
+                    // viewAllVehicles()
                     break;
             }
         }
@@ -76,8 +123,8 @@ public class UsedCarDealership {
      * @return a list of vehicles loaded from file
      */
     private static List<Vehicle> initializeListVehicle(String filePath) {
-            VehicleFileHandler vehicleLoader = new VehicleFileHandler(filePath);
-            return vehicleLoader.load();
+        VehicleFileHandler vehicleLoader = new VehicleFileHandler(filePath);
+        return vehicleLoader.load();
     }
 
     /**
@@ -87,8 +134,8 @@ public class UsedCarDealership {
      * @return a list of customers loaded from file
      */
     private static List<Customer> initializeListCustomer(String filePath) {
-            CustomerFileHandler customerLoader = new CustomerFileHandler(filePath);
-            return customerLoader.load();
+        CustomerFileHandler customerLoader = new CustomerFileHandler(filePath);
+        return customerLoader.load();
     }
 
     /**
@@ -98,8 +145,8 @@ public class UsedCarDealership {
      * @return a list of transactions loaded from file
      */
     private static List<Transaction> initializeListTransaction(String filePath) {
-            TransactionFileHandler transactionLoader = new TransactionFileHandler(filePath);
-            return transactionLoader.load();
+        TransactionFileHandler transactionLoader = new TransactionFileHandler(filePath);
+        return transactionLoader.load();
     }
 
     private static void shutdown() {
