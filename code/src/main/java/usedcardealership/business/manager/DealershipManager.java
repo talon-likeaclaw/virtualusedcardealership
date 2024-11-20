e890cc09ad47c7d3c5a6ba6e51ed896c6af65630
talonlikeaclaw <talonlikeaclaw@proton.me>
2024-11-20 14:22:32 -0500

    add functions to get vehicles by type

--- code/src/main/java/usedcardealership/business/manager/DealershipManager.java
+++ code/src/main/java/usedcardealership/business/manager/DealershipManager.java
@@ -7,9 +7,11 @@
 
 package usedcardealership.business.manager;
 
+import usedcardealership.business.filter.VehicleTypeFilter;
 import usedcardealership.data.customer.*;
 import usedcardealership.data.transaction.*;
 import usedcardealership.data.vehicle.*;
+import usedcardealership.business.filter.*;
 
 import java.util.*;
 
@@ -64,6 +66,36 @@ public class DealershipManager {
     return this.vehicleManager.getDatabase();
   }
 
+  public List<Vehicle> getCars() {
+    IFilter<Vehicle> carFilter = new VehicleTypeFilter("Car");
+    return this.vehicleManager.searchInventory(carFilter);
+  }
+
+  public List<Vehicle> getSUVs() {
+    IFilter<Vehicle> suvFilter = new VehicleTypeFilter("SUV");
+    return this.vehicleManager.searchInventory(suvFilter);
+  }
+
+  public List<Vehicle> getVans() {
+    IFilter<Vehicle> vanFilter = new VehicleTypeFilter("Van");
+    return this.vehicleManager.searchInventory(vanFilter);
+  }
+
+  public List<Vehicle> getRVs() {
+    IFilter<Vehicle> rvFilter = new VehicleTypeFilter("RV");
+    return this.vehicleManager.searchInventory(rvFilter);
+  }
+
+  public List<Vehicle> getMotorcycles() {
+    IFilter<Vehicle> motorcycleFilter = new VehicleTypeFilter("Motorcycle");
+    return this.vehicleManager.searchInventory(motorcycleFilter);
+  }
+
+  public List<Vehicle> getTrucks() {
+    IFilter<Vehicle> truckFilter = new VehicleTypeFilter("PickupTruck");
+    return this.vehicleManager.searchInventory(truckFilter);
+  }
+
   /**
    * Method for adding or removing money from the dealership's account balance.
    * 
@@ -73,4 +105,5 @@ public class DealershipManager {
   public void updateAccountBalance(double balanceChange) {
     this.accountBalance += balanceChange;
   }
+
 }
