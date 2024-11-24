5d350857c4942d0e56b196f6601f0b0fc5daea3e
talonlikeaclaw <talonlikeaclaw@proton.me>
2024-11-23 19:18:09 -0500

    add comments and refine color printing methods

--- code/src/main/java/usedcardealership/interaction/PrettyUtils.java
+++ code/src/main/java/usedcardealership/interaction/PrettyUtils.java
@@ -5,7 +5,6 @@ public class PrettyUtils {
     private static final String RED = "\033[31m";
     private static final String GREEN = "\033[32m";
     private static final String YELLOW = "\033[33m";
-    private static final String BLUE = "\033[34m";
     private static final String CYAN = "\033[36m";
 
     /**
@@ -20,25 +19,57 @@ public class PrettyUtils {
 
     /**
      * Prints a message in red
+     * 
+     * @param message the message to print
      */
     public static void printRed(String message) {
         printColor(message, RED);
     }
 
     /**
+     * Returns a message in red
+     * 
+     * @param message the message to covert
+     * @return String- the message in red
+     */
+    public static String returnRed(String message) {
+        return RED + message + RESET;
+    }
+
+    /**
      * Prints a message in green
+     * 
+     * @param message the message to print
      */
     public static void printGreen(String message) {
         printColor(message, GREEN);
     }
 
     /**
+     * Returns a message in green
+     * 
+     * @param message the message to covert
+     * @return String- the message in green
+     */
+    public static String returnGreen(String message) {
+        return GREEN + message + RESET;
+    }
+
+    /**
      * Prints a message in yellow
+     * 
+     * @param message the message to print in yellow
      */
     public static void printYellow(String message) {
         printColor(message, YELLOW);
     }
 
+    /**
+     * Returns a message in yellow
+     * 
+     * @param message the message to convert
+     * @return String - the message in yellow
+     */
     public static String returnYellow(String message) {
         return YELLOW + message + RESET;
     }
@@ -53,28 +84,32 @@ public class PrettyUtils {
     }
 
     /**
-     * Prints a message in blue
-     */
-    public static void printBlue(String message) {
-        printColor(message, BLUE);
-    }
-
-    /**
      * Prints a message in cyan
+     * @param message the message to print
      */
     public static void printCyan(String message) {
         printColor(message, CYAN);
     }
 
+    /**
+     * Returns a message in cyan
+     * 
+     * @param message the message to convert
+     * @return String - the message in cyan
+     */
+    public static String returnCyan(String message) {
+        return CYAN + message + RESET;
+    }
+
     public static String returnCars() {
         return "      ______                   ______\n" +
                 "   __//__|__\\___           ___/__|__\\\\___\n" +
-                " _|  _     _    |          |  _     _    |_\n" +
-                "|_|-(_)-------(_)----------(_)-------(_)-|_|\n";
+                " _|  _     _    |         |    _     _  |_\n" +
+                "|_|-(_)-------(_)---------(_)-------(_)-|_|\n";
     }
 
     /**
-     * Wipes the console screen
+     * Wipes the console screen0
      */
     public static void wipe() {
         System.out.print("\033[H\033[2J");
