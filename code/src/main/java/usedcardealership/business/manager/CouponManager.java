1abc2f9d75ba7585db87bb1980f19efd001ed39a
unknown <jseb1401@gmail.com>
2024-11-25 23:36:32 -0500

    Changing odds of coupon to 25%

--- code/src/main/java/usedcardealership/business/manager/CouponManager.java
+++ code/src/main/java/usedcardealership/business/manager/CouponManager.java
@@ -90,7 +90,7 @@ public class CouponManager {
         Random random = new Random();
         double chance = random.nextDouble();
     
-        if (chance <= 1) { // 25% chance
+        if (chance <= 0.25) { // 25% chance
             if (!this.couponList.isEmpty()) {
                 return this.couponList.get(random.nextInt(this.couponList.size()));
             } else {
